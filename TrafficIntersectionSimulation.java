24676d5d53fd900328850d1111da007aa21b5944
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-09-17 12:56:11 +0200

    add driving vehicles to TrafficLane

--- TrafficIntersectionSimulation.java
+++ TrafficIntersectionSimulation.java
@@ -2,5 +2,41 @@ public class TrafficIntersectionSimulation {
 
   public static void main(String[] args) {
 
+    TrafficLane lane1 = new TrafficLane("North-South Lane", 5);
+    TrafficLane lane2 = new TrafficLane("East-West Lane", 4);
+    TrafficLane lane3 = new TrafficLane("Diagonal Lane", 3);
+
+    lane1.start();
+    try {
+      lane1.join();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+//    try {
+//      Thread.sleep(1000);
+//    } catch (InterruptedException e) {
+//      throw new RuntimeException(e);
+//    }
+
+    lane2.start();
+    try {
+      lane2.join();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+//    try {
+//      Thread.sleep(1000);
+//    } catch (InterruptedException e) {
+//      throw new RuntimeException(e);
+//    }
+
+    lane3.start();
+    try {
+      lane3.join();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+    System.out.println("Night falls on the intersection and there are no vehicles left ...");
   }
 }
--- TrafficLane.java
+++ TrafficLane.java
@@ -1,4 +1,27 @@
+import java.util.Random;
+
 public class TrafficLane extends Thread {
 
+  private final String lane;
+  private final int vehiclesNo;
+  private final Random random = new Random();
 
+  public TrafficLane(String lane, int vehiclesNo) {
+    this.lane = lane;
+    this.vehiclesNo = vehiclesNo;
+  }
+
+  @Override
+  public void run() {
+    System.out.println(lane + " is starting...");
+    for (int i = 1; i < vehiclesNo; i++) {
+      System.out.println(lane + ": Vehicle " + i + " enters the intersection");
+    }
+    try {
+      Thread.sleep(500 + random.nextInt(0, 400));
+    } catch (InterruptedException e) {
+      System.out.println("Thread interrupted: " + e);
+    }
+    System.out.println(lane + " is ending...");
+  }
 }
