5e8277ed4face83540f9744bf5a1242641d62cdc
Natalie Lazarev <lazarev.n@yahoo.com>
2024-09-18 12:19:10 +0300

    improve solution: synchronized() & notify()

--- TrafficIntersectionSimulation.java
+++ TrafficIntersectionSimulation.java
@@ -1,19 +1,21 @@
 public class TrafficIntersectionSimulation {
 
     public static void main(String[] args) {
-        TrafficLane northLine = new TrafficLane("North-South Lane");
-        TrafficLane southLine = new TrafficLane("East-West Lane");
-        TrafficLane diagonalLane = new TrafficLane("Diagonal Lane");
+        Object intersectionLock = new Object();
+
+        TrafficLane northLine = new TrafficLane("North-South Lane", intersectionLock);
+        TrafficLane southLine = new TrafficLane("East-West Lane", intersectionLock);
+        TrafficLane diagonalLane = new TrafficLane("Diagonal Lane", intersectionLock);
+
+        northLine.start();
+        southLine.start();
+        diagonalLane.start();
 
         try {
-            northLine.start();
             northLine.join();
-
-            southLine.start();
             southLine.join();
-
-            diagonalLane.start();
             diagonalLane.join();
+
         } catch (InterruptedException e) {
             System.out.println("Simulation was interrupted.");
         }
--- TrafficLane.java
+++ TrafficLane.java
@@ -1,10 +1,12 @@
 import java.util.Random;
 
 class TrafficLane extends Thread {
-    private String laneName;
+    private final String laneName;
+    private final Object intersectionLock;
 
-    public TrafficLane(String laneName) {
+    public TrafficLane(String laneName, Object intersectionLock) {
         this.laneName = laneName;
+        this.intersectionLock = intersectionLock;
     }
 
     @Override
@@ -15,13 +17,24 @@ class TrafficLane extends Thread {
             try {
                 Thread.sleep(random.nextInt(500));
 
-                System.out.printf("Vehicle %d from %s enters the intersection.\n", i, laneName);
+                // Ensure only one vehicle is in the intersection at a time
+                synchronized (intersectionLock) {
+                    System.out.printf("Vehicle %d from %s enters the intersection.\n", i, laneName.toUpperCase());
+
+                    // Simulate the time it takes for the vehicle to cross the intersection
+                    Thread.sleep(1000);
+
+                    System.out.printf("Vehicle %d from %s exits the intersection.\n", i, laneName.toUpperCase());
+
+                    // Notify other waiting threads that the intersection is available
+                    intersectionLock.notify();
+                }
 
             } catch (InterruptedException e) {
-                System.err.println(laneName + " was interrupted.");
+                System.err.println(laneName.toUpperCase() + " was interrupted.");
             }
         }
 
-        System.out.println("All vehicles from " + laneName + " have passed.");
+        System.out.println("All vehicles from " + laneName.toUpperCase() + " have passed.");
     }
 }
